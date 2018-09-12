ddde06087533e36815488963ed675857d0fdaabe
ivanguillermo <ivanglopezp@gmail.com>
2018-09-12 20:43:15 -0300

    player completed

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -6,57 +6,66 @@ const toggle = player.querySelector('.toggle');
 //const skipButtons = player.querySelectorAll('[data-skip]');
 const speedBtn = document.getElementById('speed');
 const ranges = player.querySelectorAll('.player_slider');
+let currentTime = video.currentTime;
 
+
+video.volume = 0.2;
+progressBar.style.flexBasis = `0%`;
 
 function togglePlay(){
 	const method = video.paused ? 'play' : 'pause';
 	video[method]();
+	console.log(currentTime);
 
 }
 
 function skip(t){
-	console.log('3');
 	//video.currentTime += parseFloat(this.dataset.skip);
 	video.currentTime += t;
+	console.log(video.currentTime);
 }
 
 function handleRangeUpdate(){
 	video[this.name] = this.value;
+	console.log(video.currentTime);
 }
 
 function scrub(e){
 	const scrubTime= (e.offsetX / progress.offsetWidth) * video.duration;
 	video.currentTime = scrubTime;
-
+	console.log(video.currentTime);
 }
 function resetSpeed(){
 	video.playbackRate = 1;
 	speedBtn.value = 1;
+	console.log(video.currentTime);
 }
 
 function updateButton(){
 	const icon = this.paused ? '►' : '=';
 	toggle.textContent = icon;
+	console.log(video.currentTime);
 }
 
 function handleProgress(){
-	const percent = (video.currentTime / video.duration)*100;
-	progressBar.style.flexBasis = `${percent}`+'%';
-	progressBar.style.width = `${percent}`+'%';
-	progressBar.style.background = '#c600ff';
+	const percent = (video.currentTime / video.duration) * 100;
+	progressBar.style.flexBasis = `${percent}%`;
+	progressBar.style.width = `${percent}%`;
+	progressBar.style.backgroundColor = '#C600ff';
+	console.log(video.currentTime + ' handleProgress');
 }
 
 
 function fullScreening() {
 	video.mozRequestFullScreen();
-	video.webkitRequestFullScreen();
-	video.requestFullScreen();
+	video.requestFullscreen();
+	console.log(currentTime);
 }
 
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
-toggle.addEventListener('timeupdate', handleProgress);
+video.addEventListener('timeupdate', handleProgress);
 toggle.addEventListener('click', togglePlay);
 //skipButtons.forEach(button => button.addEventListener('click', skip ));
 ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
